590d0558b595f9f5c7094a68aac9a8d47da0c3e6
Augusto Furlan <gulyfurlan@gmail.com>
2019-04-28 13:05:22 -0300

    busca endereco

---
+++ app/Http/Controllers/API/V1/EnderecoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Endereco;
+use App\Models\SysEstadoBR;
+use App\Models\SysCidadeBR;
+
+class EnderecoController extends Controller {
+    
+    public function busca_estado(Request $request) {
+        $estado = SysEstadoBR::where('nome', $request->nome)->first();
+        return response()->json(compact('estado'));
+    }
+
+    public function busca_cidade (Request $request) {
+        if($request->nome == null) {
+            $list = SysCidadeBR::where('estado_id', $request->estado_id)->orderBy('nome', 'asc')->get();
+            return response()->json(compact('list'));
+        } else {
+            $list = SysCidadeBR::where('estado_id', $request->estado_id)->where('nome', $request->nome)->first();
+            return response()->json(compact('list'));
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,12 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('auth/login', "API\V1\authController@authenticate");
 Route::post('auth/register', "API\V1\authController@register");
-Route::post('endereco/store', "API\V1\\enderecoController@store_endereco");
+
+Route::get('endereco/busca-estado/', "API\V1\\EnderecoController@busca_estado");
+Route::get('endereco/busca-cidade/', "API\V1\\EnderecoController@busca_cidade");
 
 Route::group(['middleware' => 'jwt.auth', 'prefix' => 'empresas'], function () {
 
