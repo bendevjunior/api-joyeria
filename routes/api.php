d9a6bc444ca45d2b238b5a0bffda8af24416e418
PrincJr <junorsilvaasafe@gmail.com>
2019-04-30 22:51:11 -0400

    vamos ver se vai esse cors agora pqp

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,17 @@ Route::group(['middleware' => 'jwt.auth','cors' ,  'prefix' => 'auth'], function
     Route::post('ativar-conta', "API\V1\authController@ativar_conta");
 });
 
-Route::group(['middleware' => 'jwt.auth', 'prefix' => 'fornecedor'], function () {
+Route::group(['middleware' => 'jwt.auth', 'cors','prefix' => 'fornecedor'], function () {
     Route::post('store', "API\V1\FornecedorController@store");
     Route::get('show', "API\V1\FornecedorController@show");
     Route::post('update', "API\V1\FornecedorController@update");
 });
 
-Route::group(['middleware' => 'jwt.auth', 'prefix' => 'produto'], function () {
+Route::group(['middleware' => 'jwt.auth', 'cors', 'prefix' => 'produto'], function () {
     Route::post('store', "API\V1\ProdutoController@store");
     Route::post('store_update', "API\V1\ProdutoController@store_update_fornecedor");
 });
 
-Route::group(['middleware' => 'jwt.auth', 'prefix' => 'produto/compra'], function () {
+Route::group(['middleware' => 'jwt.auth','cors','prefix' => 'produto/compra'], function () {
     Route::post('store', "API\V1\ProdutoCompraController@store");
 });
