f8d15a0afd07ad061fd7fec8a3207e1de072ba09
PrincJr <junorsilvaasafe@gmail.com>
2019-06-12 12:09:34 -0400

    slider mudancas no update

--- app/Http/Controllers/API/V1/SliderController.php
+++ app/Http/Controllers/API/V1/SliderController.php
@@ -50,7 +50,7 @@ class SliderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
         $slider = Slider::find($request->id);
         $slider->update($request->all());
