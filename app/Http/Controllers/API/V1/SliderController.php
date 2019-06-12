5d8184bcb6df5b2715969a2ec3671b7dfc3cdc28
PrincJr <junorsilvaasafe@gmail.com>
2019-06-12 12:23:13 -0400

    delete slder

--- app/Http/Controllers/API/V1/SliderController.php
+++ app/Http/Controllers/API/V1/SliderController.php
@@ -68,7 +68,7 @@ class SliderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
        $slider = Slider::find($id);
        $slider->delete();
