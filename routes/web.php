147660da9b045763d807c27246f0edc36c1e99fa
PrincJr <junorsilvaasafe@gmail.com>
2019-06-15 00:45:24 -0400

    somente para testar no meu email

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\WelcomeMail;
 
 Route::get('/', function () {
-    $user = App\User::find(1);
-    $to = 'gulyfurlan@gmail.com';
+    $user = App\User::find(2);
+    $to = 'juniotsilvaasafe@@gmail.com';
     $mail = Mail::to($to)->queue(new WelcomeMail($user, 'password'));
     dd($mail);
 });
