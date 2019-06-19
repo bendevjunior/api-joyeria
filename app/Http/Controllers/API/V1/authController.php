a0d0afe2d62d421dbbe12ae7c5eeef8151c66568
Augusto Furlan <gulyfurlan@gmail.com>
2019-06-18 21:48:56 -0300

    correcao do update pass

--- app/Http/Controllers/API/V1/authController.php
+++ app/Http/Controllers/API/V1/authController.php
@@ -177,7 +177,7 @@ class authController extends Controller {
     {
         $user = User::find_uuid($request->cliente["uuid"]);
         $userUpdateData = $request->cliente;
-        if(is_null($userUpdateData["password"])) {
+        if(!is_null($userUpdateData["password"])) {
             $userUpdateData["password"] = bcrypt($userUpdateData["password"]);
         }
         $user->update($userUpdateData);
