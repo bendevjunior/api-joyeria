02814d6105936e436e58316446b29341e63f72c9
PrincJr <junorsilvaasafe@gmail.com>
2019-06-20 23:23:00 -0400

    mudancas nos metodos de recebimento

--- app/Http/Controllers/API/V1/ProdutoController.php
+++ app/Http/Controllers/API/V1/ProdutoController.php
@@ -213,11 +213,11 @@ class ProdutoController extends Controller
     public function update(Request $request)
     {
         $produto = Produto::find_uuid($request->uuid);
-        $colecao = ProdutoColecao::find_uuid($request->colecao_uuid);
+        $colecao = ProdutoColecao::find_uuid($request->produto['colecao_uuid']);
         if(!is_null($colecao)){
             $produto['colecao_id'] = $colecao->id;
         }
-        $categoria = ProductCategory::find_uuid($request->produto->categoria_uuid);
+        $categoria = ProductCategory::find_uuid($request->produto['categoria_uuid']);
         if(!is_null($categoria)){
             $produto['categoria_id'] = $categoria->id;
         }
