e89b6f6e265eaa836722b736f51e4b3afea5f2f6
Augusto Furlan <gulyfurlan@gmail.com>
2019-09-11 17:52:39 -0300

    correcao na image

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,19 +6,25 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function base64ToImage($base64_string, $output_file) {
-        $file = fopen($output_file, "wb");
-    
+        //$file = fopen($output_file, "w");
+        //$file = fopen('public/f.png', "w");
+
         $data = explode(',', $base64_string);
-    
-        fwrite($file, base64_decode($data[1]));
-        fclose($file);
-    
-        return $output_file;
+
+        //fwrite($file, base64_decode($data[1]));
+        //fclose($file);
+        $data = base64_decode($data[1]);
+
+        Storage::put($output_file, $data);
+
+
+        return storage_path($output_file);
     }
 }
