e59bda91c0e75c2f05faf8cab25c9f0f6144c892
PrincJr <junorsilvaasafe@gmail.com>
2019-09-12 15:10:22 -0400

    aa

--- app/Http/Controllers/API/V1/ProdutoController.php
+++ app/Http/Controllers/API/V1/ProdutoController.php
@@ -63,9 +63,9 @@ class ProdutoController extends Controller
 
         $produto = $request["produto"];
        // $produto->codigo_de_barras = 11111;// str_pad($i, 13, '0', STR_PAD_LEFT);
-       return response()->json($produto['primeira_imagem']);
+       
         $img_name = (string) Str::uuid() . '.png';
-        $produto->primeira_imagem = $this->base64ToImage($request['produto']['primiera_imagem'], 'img/'.$img_name);
+        $produto['primeira_imagem'] = $this->base64ToImage($produto['primeira_imagem'], 'img/'.$img_name);
         $produto = Produto::create($produto);
         if ($request["fotos"] != null) {
             foreach ($request["fotos"] as $foto) {
