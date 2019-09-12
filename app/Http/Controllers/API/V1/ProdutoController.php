6020c294d5583c60e91df9d817092f91d3d10825
PrincJr <junorsilvaasafe@gmail.com>
2019-09-11 21:39:26 -0400

    update img

--- app/Http/Controllers/API/V1/ProdutoController.php
+++ app/Http/Controllers/API/V1/ProdutoController.php
@@ -78,12 +78,12 @@ class ProdutoController extends Controller
 
 
         $produto = $request["produto"];
-        $produto = Produto::create($produto);
+      
         $produto->numero_codigo_de_barras = str_pad($produto->id, 13, '0', STR_PAD_LEFT);
         $produto->codigo_de_barras = str_pad($produto->id, 13, '0', STR_PAD_LEFT);
         $img_name = (string) Str::uuid() . '.png';
         $produto = $this->base64ToImage($produto->primeira_imagem, 'img/'.$img_name);
-        $produto->save();
+        $produto = Produto::create($produto);
         $produto = Produto::find($produto->id);
         if ($request["fotos"] != null) {
             foreach ($request["fotos"] as $foto) {
